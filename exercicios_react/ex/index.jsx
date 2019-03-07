b7ecda6179f8887646362a3ea1fde977d3d75ce3
gabriela conde <gabiconde97@gmail.com>
2019-03-07 18:38:56 -0300

    Ola react

---
+++ exercicios_react/ex/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+//ReactDom.render(<h1>Olá React</h1>, document.getElementById('app'))
+ReactDom.render(<h1>Olá React2</h1>, document.querySelector('#app'))
